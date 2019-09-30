3bef4f76b3cbd76f644076bb9d5854b4b90c56c9
LeeHanul <kkak1072@gmail.com>
2019-10-01 05:20:05 +0900

    error handling test

--- main.js
+++ main.js
@@ -4,13 +4,7 @@ const { context } = require('@actions/github');
 async function main() {
   const prNumber = context.payload.pull_request.number;
 
-  console.log(prNumber);
-
-  if (typeof prNumber !== 'number') {
-    throw new Error("pull request number is not found.");
-  }
-
-  core.setOutput('pr', prNumber);
+  throw new Error("pull request number is not found.");
 }
 
 main().catch(err => core.setFailed(err.message));
